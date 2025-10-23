20f0ab3cca22ad306d2b83896da3eecad5005643
Vanh190404 <AnhNV.B22PT015@stu.ptit.edu.vn>
2025-10-24 00:51:17 +0700

    connectDB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,34 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+
+const MONGODB_URI = 'mongodb+srv://vanishadmin:W8njoin6KTOHJO3X@clustervanish.gmoqtvc.mongodb.net/?retryWrites=true&w=majority&appName=ClusterVanish'
+
+const DB_NAME = 'trello-vanish'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDBInstance = null
+
+const clientInstance = new MongoClient( MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await clientInstance.connect()
+  trelloDBInstance = clientInstance.db( DB_NAME )
+}
+
+export const GET_DB = () => {
+  if ( !trelloDBInstance ) throw new Error( 'MongoDB is not connected yet!' )
+  return trelloDBInstance
+}
+
+export const CLOSE_DB = async () => {
+  await clientInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -2,31 +2,48 @@
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
+ * eslint-disable-next-line no-console
  */
-
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'exit-hook'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb.js'
 
-const app = express()
+const startServer = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', (req, res) => {
+    // Test Absolute import mapOrder
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    console.log(`Hello Vanish, I am running at ${ hostname }:${ port }/`)
+  })
+
+  exitHook( () => {
+    CLOSE_DB()
+  })
+}
+
+( async () => {
+  try {
+    console.log('Connecting to MongoDB...')
+    await CONNECT_DB()
+    console.log('MongoDB connected successfully!')
+    startServer()
+  }
+  catch (err) {
+    console.error(err)
+    process.exit(0)
+  }
+})()
+// CONNECT_DB()
+//   .then(() => { console.log('Connected to MongoDB') })
+//   .then(() => { startServer() })
+//   .catch(err => {
+//     console.error(err)
+//     process.exit(0)
+//   })
