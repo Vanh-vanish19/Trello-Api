916d5179fbc097a19ed3921f1f42bda273de7d10
Vanh190404 <AnhNV.B22PT015@stu.ptit.edu.vn>
2025-11-08 22:22:31 +0700

    done_delete_col_card

--- src/models/cardModel.js
+++ src/models/cardModel.js
@@ -62,10 +62,19 @@ const update = async (cardId, updateData) => {
   }
 }
 
+const deleteManyById = async (columnId) => {
+  try {
+    const result = await GET_DB().collection(CARD_COLLECTION_NAME).deleteMany({ columnId: new ObjectId(columnId) })
+    // console.log(result)
+    return result
+  } catch (error) {throw new Error(error)}
+}
+
 export const cardModel = {
   CARD_COLLECTION_NAME,
   CARD_COLLECTION_SCHEMA,
   createNew,
   findOneById,
-  update
+  update,
+  deleteManyById
 }
--- src/services/columnService.js
+++ src/services/columnService.js
@@ -1,6 +1,8 @@
 //import ApiError from '~/utils/ApiError'
 import { boardModel } from '~/models/boardModel'
 import { columnModel } from '~/models/columnModel'
+import { cardModel } from '~/models/cardModel'
+import ApiError from '~/utils/ApiError'
 const createNew = async(reqBody) => {
   // eslint-disable-next-line no-useless-catch
   try {
@@ -35,7 +37,28 @@ const update = async (columnId, reqBody) => {
   }
 }
 
+
+const deleteItem = async (columnId) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const targetColumn = await columnModel.findOneById(columnId)
+    if (!targetColumn) {
+      throw new ApiError(404, 'Column not found')
+    }
+    // xóa column
+    await columnModel.deleteOneById(columnId)
+    // xóa cards
+    await cardModel.deleteManyById(columnId)
+    // update columnOrderIds
+    await boardModel.pullColumnOrderIds(targetColumn)
+    return { result :'delete success' }
+  } catch (err) {
+    throw err
+  }
+}
+
 export const columnService = {
   createNew,
-  update
+  update,
+  deleteItem
 }
